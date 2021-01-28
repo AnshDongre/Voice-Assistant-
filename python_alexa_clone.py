510d872b37a1bdb5f5fafe5b7fac359c5797b9db
AnshDongre <74485993+AnshDongre@users.noreply.github.com>
2021-01-28 22:34:15 +0530

    Add files via upload

---
+++ python_alexa_clone.py
@@ -0,0 +1,36 @@
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as saurce:
+            print('mai sun raha hu...')
+            voice = listener.listen(saurce)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+
+            print(command)
+
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    voice = take_command()
+    if 'play' in voice:
+        song = voice.replace('play', '')
+        talk('playing'+song)
+        pywhatkit.playonyt(song)
+    if 'search' in voice:
+        search = voice.replace('search', '')
+        talk('searching'+search)
+        pywhatkit.info(search, 3)
+    if 'time' in voice:
+        time = datetime.datetime.now().strftime('%H:%M')
+        talk('current time is'+time)
+
+
+run_alexa()
